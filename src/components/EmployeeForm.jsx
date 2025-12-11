9e8ee199329310100a22252d5155efd3d4ab1bc6
kaburbank <kaburbank@gmail.com>
2025-12-11 12:15:10 -0700

    Update EmployeeForm.jsx

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -7,10 +7,10 @@ function EmployeeForm({}) {
   const [password, setPassword] = useState("");
   return (
     <form className="NewItem">
-     <input type="text" name="firstName" placeholder="First Name" />
-      <input type="text" name="lastName" placeholder="Last Name"/>
-      <input type="text" name="role" placeholder="Role Name"/>
-      <input type="text" name="password" placeholder="Password"/>
+     <input type="text" name="firstName" placeholder="First Name" onChange = {(e)=>setFirstName(e.target.value)} />
+      <input type="text" name="lastName" placeholder="Last Name" onChange = {(e)=>setLastName(e.target.value)} />
+      <input type="text" name="role" placeholder="Role Name" onChange = {(e)=>setRole(e.target.value)} />
+      <input type="text" name="password" placeholder="Password" onChange = {(e)=>setPassword(e.target.value)} />
       <button type="submit">Add Employee</button>
     </form>
   );
